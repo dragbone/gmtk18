3a33f931e84e00a26c63e9127ec78cbbb53c62d7
Sascha Scherrer <ddragbone@gmail.com>
2018-09-01 00:52:54 +0200

    Added player & movement

---
+++ gmtk18/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Enemy;
+
+    void Update()
+    {
+        var direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector3.back;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+        }
+
+        var eulerAngles = Quaternion.LookRotation(Enemy.transform.position - transform.position, Vector3.up)
+            .eulerAngles;
+        Player.transform.rotation = Quaternion.Euler(0f, eulerAngles.y, 0f);
+        Player.transform.Translate(direction * Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(eulerAngles.x, eulerAngles.y, eulerAngles.z);
+    }
+}
