ff1f45759faa8bf9060a89f78abc3e7a19809e99
Peter Camastral <petercamastral@gmx.ch>
2018-09-01 21:12:01 +0200

    add worldcanvas to enemy prefabs and make it look at camera

---
+++ gmtk18/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+
+	// Use this for initialization
+	public Camera cameraToLookAt;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var v = cameraToLookAt.transform.position - transform.position;
+		v.x = v.z = 0.0f;
+		transform.LookAt( cameraToLookAt.transform.position - v); 
+		transform.Rotate(0,180,0);
+	}
+}
